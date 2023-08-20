e613a56ed711f33a28a0d43740b00f87b63bf7eb
HardikSingh2801 <hardiksingh2002286@gmail.com>
2023-08-20 19:45:01 +0530

    commit

---
+++ Javascript/script1.js
@@ -0,0 +1,61 @@
+console.log('Hey Hardik Singh');
+
+{
+    
+var b=10;
+
+}
+
+console.log(b)
+
+
+{
+    let a=5;
+console.log(a);
+
+}
+
+// const num =15;
+// num=6;
+// let firstName ='Hardik';
+// console.log(firstName);
+
+// firstName=6;
+// console.log(firstName);
+
+// console.log(3!==3)
+
+let age=17;
+let Status =(age>=18)? 'Yes Vote' : ' Cannot vote';
+
+console.log(Status);
+
+let marks=98;
+
+if(marks>=90 && marks<=100){
+    console.log('A');
+}
+else if(marks >=80 && marks<90 ){
+    console.log('B');
+}
+if(marks>=70 && marks<80){
+    console.log('C');
+}
+if(marks>=60 && marks<70){
+    console.log('D');
+}
+if(marks>=50 && marks<60){
+    console.log('E');
+}
+
+let num=2 
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    case 4: console.log('D');
+    break;
+}
