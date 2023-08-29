5a2dd1bd3ae86f0abdf6c9f80bc6151d9d52cf30
HardikSingh2801 <hardiksingh2002286@gmail.com>
2023-08-29 17:48:13 +0530

    js3

---
+++ Javascript/script3.js
@@ -0,0 +1,22 @@
+console.log('Basics 3')
+
+// let lastName= 'Babbar';
+
+// let firstName = new String ('Hardik');
+
+// let message = `this is
+// my first 
+// message`
+
+// console.log(message);
+
+
+// let date = new Date();
+
+// let date2 = new Date(' 28 Jan 2002 4:30');
+
+// console.log(date2);
+
+let numbers= [1,3,5,7];
+
+console.log(numbers);
